455a546c54f2f80e1c01f2b7833f8be3f8637b77
ralp99 <ralp99@hotmail.com>
2022-04-08 03:50:41 -0700

    migrate project files from Transported folder

---
+++ Assets/Scripts/RAGameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class RAGameManager : MonoBehaviour
+{
+
+    public static RAGameManager Instance;
+
+    public UnityAction FireButtonAction;
+    public bool FireButtonPressed;
+
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (FireButtonPressed)
+        {
+            FireButtonPressed = false;
+            FireButtonAction();
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/connector.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class connector : MonoBehaviour {
+
+
+    public GameObject LeftHandNode, RightHandNode;
+    public bool UseLocalPos;
+
+	// Use this for initialization
+	void Start () {
+
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (UseLocalPos)
+        {
+            LeftHandNode.transform.localPosition = InputTracking.GetLocalPosition(XRNode.LeftHand);
+            RightHandNode.transform.localPosition = InputTracking.GetLocalPosition(XRNode.RightHand);
+        }
+
+        else
+        {
+            LeftHandNode.transform.position = InputTracking.GetLocalPosition(XRNode.LeftHand);
+            RightHandNode.transform.position = InputTracking.GetLocalPosition(XRNode.RightHand);
+        }
+
+       
+
+        LeftHandNode.transform.localRotation = InputTracking.GetLocalRotation (XRNode.LeftHand);
+        RightHandNode.transform.localRotation = InputTracking.GetLocalRotation(XRNode.RightHand);
+
+
+    }
+}
